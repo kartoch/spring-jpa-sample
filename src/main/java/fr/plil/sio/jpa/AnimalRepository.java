1447d5210658792afc28af58dfd93eebf4e8e7e7
Julien Iguchi-Cartigny <julien.iguchi-cartigny@univ-lille1.fr>
2015-05-22 11:39:28 +0200

    Switching animal repository to spring data JPA

--- src/main/java/fr/plil/sio/jpa/AnimalRepository.java
+++ src/main/java/fr/plil/sio/jpa/AnimalRepository.java
@@ -1,44 +1,8 @@
 package fr.plil.sio.jpa;
 
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnimalRepository {
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
 
-    /**
-     * Save an animal instance in the database.
-     *
-     * @param animal the instance of animal to save.
-     * @return the animal instance
-     */
-    Animal save(Animal animal);
-
-    /**
-     * Remove the animal instance from the database.
-     *
-     * @param animal the animal to delete from the database.
-     */
-    void delete(Animal animal);
-
-    /**
-     * Return an animal instance with specified name.
-     *
-     * @param name the name of the animal to return.
-     * @return the animal instance or null if not found
-     */
     Animal findByName(String name);
-
-    /**
-     * Return an animal instance with specified id.
-     *
-     * @param id the id of the animal to return.
-     * @return the animal instance or null if not found
-     */
-    Animal findOne(Long id);
-
-    /**
-     * Return all animals in the database.
-     *
-     * @return The list of animals.
-     */
-    List<Animal> findAll();
 }
